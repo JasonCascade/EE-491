b0b9f01bbd858880f7daa3d281a6d765797e0690
Gomchiii <102383519+Gomchiii@users.noreply.github.com>
2022-04-01 11:59:47 -0700

    Update Pressure_Interrupt.ino

--- Pressure_Interrupt.ino
+++ Pressure_Interrupt.ino
@@ -1,5 +1,15 @@
+int PressureChangePin = 3;
+int state = 0;
+
+void changeState(state, PressureChangePin) {
+   state++; 
+}
+
 void setup() {
   // put your setup code here, to run once:
+  attachInterrupt(PressureChangePin, changeState, RISING);
+  
+  
 
 }
 

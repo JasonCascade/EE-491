90dbde54c302ea705c863450fd7db26211186ecf
chetkazm <73405081+chetkazm@users.noreply.github.com>
2022-04-01 12:19:31 -0700

    Update frequency.ino

--- frequency.ino
+++ frequency.ino
@@ -1,10 +1,23 @@
-//D9 pin for frequnecy change
+//D9 pin for frequency change
+int frequencypin = 9;
+volatile int freqstate = 0;
+
 void setup() {
   // put your setup code here, to run once:
-
+Serial.begin(9600);
+pinMode(frequencypin,INPUT);
+attachInterrupt(digitalPinToInterrupt(frequencypin),freq_change,RISING);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void freq_change() { 
+ freqstate = freqstate++;
+ if(freqstate==4)
+ {
+  freqstate = 0;
+ }
 }

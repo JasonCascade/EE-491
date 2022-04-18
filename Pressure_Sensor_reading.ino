9855425e9906ba26504e29eff0c6a3797a84f361
josemerc <73717936+josemerc@users.noreply.github.com>
2022-04-18 05:03:45 -0700

    Add files via upload

---
+++ Pressure_Sensor_reading.ino
@@ -0,0 +1,14 @@
+/*
+ * 
+ * ANALOG READING FROM PRESSURE SENSOR.
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

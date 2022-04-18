4e8daa9f78fedbb489ead12bada0a2148c538db9
josemerc <73717936+josemerc@users.noreply.github.com>
2022-04-18 05:10:45 -0700

    Update Pressure_Sensor_reading.ino

--- Pressure_Sensor_reading.ino
+++ Pressure_Sensor_reading.ino
@@ -3,12 +3,17 @@
  * ANALOG READING FROM PRESSURE SENSOR.
  */
 
+const int pressurePin = A0;
+double pressureVoltage = 0;
+double pressurePSI = 0;
+
 void setup() {
   // put your setup code here, to run once:
-
+serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+pressureVoltage = analogRead(pressurePin);
+serial.print(pressureVoltage);
 }

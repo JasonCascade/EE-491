3f79dc2fd499c7b7fc409567ec04bbe6456983b4
Gomchiii <102383519+Gomchiii@users.noreply.github.com>
2022-04-23 16:25:34 -0700

    Update Interrupt_to_Idle.ino

--- Interrupt_to_Idle.ino
+++ Interrupt_to_Idle.ino
@@ -1,56 +1,43 @@
-#include <avr/sleep.h> 
+const int sleepPin = 2;
+const int voltOut = 5;
+volatile int state = 0;
+bool buttonState = LOW;
+float pwm = 255;
 
-
-//const byte interruptPin = 2;   
-//volatile byte state = LOW;
-//int interruptPin
+void buttonPressed();
 
 void setup() {
+  // put your setup code here, to run once:
+  pinMode(voltOut, OUTPUT);
+  pinMode(sleepPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(sleepPin), buttonPressed, RISING);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(state == 0) {
+    pwm = 255;
+    Serial.println("Running");
+  }
+  if(state == 1) {
+    pwm = 0;
+    Serial.println("Idle");
+  }
+  else if(state > 1) {
+    state = 0;
+  }
+}
+
+void buttonPressed() {
+
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  //If interrupts >200, assume bounce and ignore.
+  if( interrupt_time - last_interrupt_time > 200)
+  {
+    state++;
+  }
+  last_interrupt_time = interrupt_time;
   
-Serial.begin(9600);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
-  pinMode(2, INPUT);     // D2 STOP/START
-  digitalWrite(2, HIGH);
-  bitClear(TIMSK0, 0);  //TC0's interrupt capability is disabled.
-// THIS may be problematic, if that is an intterupt pin we are using ^^ - JOSE
-
-}
-
-void loop()
-{
-  /* wont need this in actual implementation - JOSE
-  Serial.println("5");
-  delay(1000);
-  Serial.println("4");
-  delay(1000);
-  Serial.println("3");
-  delay(1000);
-  Serial.println("2");
-  delay(1000);
-  Serial.println("1");
-  delay(1000);
-  Serial.println("Zzz");
-  delay(1500);
-  */
-  sleepSetup();
-}
-
-void sleepSetup()
-{
-  sleep_enable();
-  attachInterrupt(2, pinInterupt, LOW);  //Nano 33 IoT interrupt pins are 2, 3, 9, 10, 11, A1, A5, A7
-  set_sleep_mode(SLEEP_MODE_IDLE);//SLEEP_MODE_STANDBY);//PWR_SAVE);//PWR_DOWN);//IDLE);
- // SMCR = SMCR & 0b00000001;
-  digitalWrite(13, LOW);
-  sleep_cpu();
-  Serial.println("Just woke up!");
-//  bitSet(TIMSK0, 0);
-  digitalWrite(13, HIGH);
-}
-
-void pinInterupt()
-{
-  sleep_disable();
-  detachInterrupt(0);
 }

eff930a77614c7ecc4d76702ad768357c6a037ba
Gomchiii <102383519+Gomchiii@users.noreply.github.com>
2022-04-23 15:32:54 -0700

    Update FanSpeed.ino

--- Contingency/FanSpeed.ino
+++ Contingency/FanSpeed.ino
@@ -1,41 +1,50 @@
 // Start at low speed. Push button > switch to next speed. Only 3 modes. If on mode 3, if button press, then go back to first mode.
-const int speedButton 3;
-const int currentOut 5;
+const int speedButton = 3;
+const int voltOut = 5;
 volatile int state = 0;
 bool buttonState = LOW;
+float pwm = 0;
 
 void buttonPressed();
 
 
 void setup() {
-  pinMode(currentOut, OUTPUT);
+  pinMode(voltOut, OUTPUT);
   pinMode(speedButton, INPUT);
-  attachInterrupt(speedButton, buttonPressed, RISING);
+  attachInterrupt(digitalPinToInterrupt(speedButton), buttonPressed, RISING);
+  Serial.begin(9600);
 }
 
 void loop() {
   //if state 0, output certain current/voltage
   if(state == 0) {
-    digitalWrite(currentOut, LOW); //maybe switch to analogWrite
+    //Fan speed at 20%
+    pwm = 51;
+    analogWrite(voltOut, pwm);
     delay(100);
+    Serial.println(state);
   }
   //if state 1, output certain current/voltage
   if(state == 1) {
-    
+    //Fan speed at 60%
+    pwm = 153;
+    analogWrite(voltOut, pwm); 
+    Serial.println(state); 
   }
   //if state 2, output certain current/voltage
   if(state == 2) {
-    
+    //Fan speed at 100%
+    pwm = 255;
+    analogWrite(voltOut, pwm); 
+    Serial.println(state); 
   }
   //if state 3, output certain current/voltage
-  if(state == 3) {
-    
-  }
-  //else if state 4, set state back to 0
-  else if(state == 4) {
+  else if(state == 3) {
+    //Set fan back to 20%
     state = 0;
+    Serial.println(state);
   }
-  
+
 }
 
 void buttonPressed() {

08093abe02f19bd644a1dc7ca5d46c4a285fa722
Gomchiii <102383519+Gomchiii@users.noreply.github.com>
2022-04-23 17:35:52 -0700

    Update FanFrequency.ino

--- Contingency/FanFrequency.ino
+++ Contingency/FanFrequency.ino
@@ -1,32 +1,88 @@
+// Start at low frequency > Push button > medium frequency > push button > high frequency > push button > back to low frequency
+const int freqButton = 19;
+//const int freqOut = 5;
+volatile int state = 0;
+bool buttonState = LOW;
 
 /*
- * code to pwm control fan.
- * Turno
+ * FAN STUFF BELOW.
  */
 
-const int FanPin = 7;
-float pwm1 = 255;
-float pwm2 = 0;
+const int FanPin = 5;
+float pwm = 255;
+//float Off = 0;
 
+const long interval_0= 1000;
+const long interval_1= 3000;
+unsigned long previousMillis = 0;
+long int c1 = 1;
+
+void buttonPressed();
 
 void setup() {
-  // put your setup code here, to run once:
-pinMode(FanPin, OUTPUT); //setting it as an output.
-
+ // pinMode(freqOut, OUTPUT);
+  pinMode(freqButton, INPUT);
+  attachInterrupt(digitalPinToInterrupt(freqButton), buttonPressed, RISING);
+  pinMode(FanPin, OUTPUT); //setting it as an output.
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    unsigned long currentMillis = millis();
+  //if state 0, frequency calculation is set to low
+  if(state == 0) {
+     if (currentMillis - previousMillis >= interval_0) {
+      previousMillis = currentMillis;
+      analogWrite(FanPin, pwm);
+          // if the LED is off turn it on and vice-versa:
+      if (pwm == 80) {
+        pwm = 255;
+      } else {
+        pwm = 80;
+      }
+    }
+  }
+  //if state 1, frequency calculation is set to medium
+  if(state == 1) {
 
-//need equation to determine this value
-analogWrite(FanPin, );
+  // Check if iteration is ODD or EVEN
+    if(c1%2) //positive value if remainder more than one.. ergo
+    {
+      if (currentMillis - previousMillis >= interval_0) {
+        c1++;
+        previousMillis = currentMillis;
+        pwm = 80;
+        analogWrite(FanPin, pwm);
+
+      }
+    }
+    else{ //else, it is even..
+      //even
+       if(currentMillis - previousMillis >= interval_1)
+      {
+        c1++;
+        previousMillis = currentMillis;
+        pwm = 255;
+        analogWrite(FanPin, pwm);
+
+      }
+    }
+     
+    
+    }
+  //else if state 2, set state back to 0
+  else if(state == 2) {
+    state = 0;
+  }
 
 }
-/*
- * Need fan to turn on and off to show frequency change.
- * 
- * 
- * 
- * 
- */
+
+void buttonPressed() {  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  //If interrupts >80, assume bounce and ignore.
+  if( interrupt_time - last_interrupt_time > 200)
+  {
+    state++;
+  }
+  last_interrupt_time = interrupt_time;
+}

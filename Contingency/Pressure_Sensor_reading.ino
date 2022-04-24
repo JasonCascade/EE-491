bd446340a8f33798790f78716ecfa9e99a56f0cb
josemerc <73717936+josemerc@users.noreply.github.com>
2022-04-23 17:37:43 -0700

    Update Pressure_Sensor_reading.ino

--- Contingency/Pressure_Sensor_reading.ino
+++ Contingency/Pressure_Sensor_reading.ino
@@ -8,14 +8,31 @@ double pressureVoltage = 0;
 double sensorVal = 0;
 double pressurePSI = 0;
 
+/*
+ * FAN stuff
+ */
+ const int FanPin = 5;
+ float pwm = 255;
+
 void setup() {
   // put your setup code here, to run once:
-serial.begin(9600);
+ pinMode(FanPin,OUTPUT);
+Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
 sensorVal = analogRead(pressurePin);
 pressureVoltage = sensorVal * (12.0/1023.0); // convert analog reading, to 0-5volts
-serial.print(pressureVoltage);
+Serial.print(pressureVoltage);
+
+  if(pressureVoltage > 8)
+  {
+    analogWrite(FanPin, 50);
+  }
+  else
+  {
+    analogWrite(FanPin,255);
+  }
 }
